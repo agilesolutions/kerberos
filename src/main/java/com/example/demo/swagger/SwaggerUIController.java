487e17001927496eae99154db59eb242a2bafe75
Robert Rong <robert.rong@agile-solutions.ch>
2019-10-12 15:59:55 +0200

    f

---
+++ src/main/java/com/example/demo/swagger/SwaggerUIController.java
@@ -0,0 +1,20 @@
+package com.example.demo.swagger;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class SwaggerUIController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+
+		return "redirect:swagger-ui.html";
+
+	}
+}
+
